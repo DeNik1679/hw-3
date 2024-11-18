80990d7b7b0277f7ed5874559fb42ef5b75fc04e
DeNik1679 <danieliskanderov57@gmail.com>
2024-11-18 14:22:02 +0600

    Add files via upload

---
+++ hw3/les3_4Month/src/components/index.js
@@ -0,0 +1,6 @@
+export { Brands } from './homePage/Brands/Brands';
+export { NevArrivals } from './homePage/NevArrivals/NevArrivals';
+export { TopSelling } from './homePage/TopSelling/TopSelling';
+export { DressStyle } from './homePage/dressStyle/DressStyle';
+export { Casual } from './categoryPage/casual/Casual';
+export { Filter } from './categoryPage/filter/Filter';
---
+++ hw3/les3_4Month/src/pages/Category.jsx
@@ -0,0 +1,16 @@
+import { Casual, Filter } from "../components";
+import '../app.css'
+
+export const Category = () => {
+
+    return (
+        <div className="container">
+            <div className="flexItem">
+                <Filter />
+                <Casual />
+            </div>
+            
+        </div>
+    );
+}
+
